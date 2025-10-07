628ef0d9aac41ade63f5131f9000ab291f663acc
j-aarush <aarush.wins.money@gmail.com>
2025-10-06 20:33:33 -0400

    velocity control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/tests/pulleyintaketest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/tests/pulleyintaketest.java
@@ -4,6 +4,9 @@ package org.firstinspires.ftc.teamcode.DECODE.tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 @Autonomous
@@ -16,8 +19,9 @@ public class pulleyintaketest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor rightintake;
-        DcMotor leftintake;
+        DcMotorEx rightintake;
+        DcMotorEx leftintake;
+        double speed;
 
 
         //
@@ -25,15 +29,18 @@ public class pulleyintaketest extends LinearOpMode {
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        rightintake = hardwareMap.get(DcMotor.class, "rightin");
-        leftintake = hardwareMap.get(DcMotor.class, "leftin");
+        rightintake = hardwareMap.get(DcMotorEx.class, "rightin");
+        leftintake = hardwareMap.get(DcMotorEx.class, "leftin");
 
-        leftintake.setDirection(DcMotor.Direction.REVERSE);
+        leftintake.setDirection(DcMotorEx.Direction.REVERSE);
 
 
 
-        rightintake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftintake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightintake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        leftintake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        leftintake.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+//        leftintake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); x
+
 
         byte speedx = 2;
         waitForStart();
@@ -47,6 +54,14 @@ public class pulleyintaketest extends LinearOpMode {
 
            leftintake.setPower(gamepad1.right_stick_x);
             rightintake.setPower(gamepad1.right_stick_x);
+
+
+            speed = leftintake.getVelocity();
+            double velocity = speed * 60 / 28;
+
+
+            telemetry.addData("speed", velocity);
+            telemetry.update();
 
 
 
