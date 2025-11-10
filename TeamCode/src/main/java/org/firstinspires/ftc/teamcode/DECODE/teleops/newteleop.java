38b608e7e3bf0d159d233eba904cc46ed37a4461
j-aarush <aarush.wins.money@gmail.com>
2025-11-09 21:17:13 -0500

    spindexer kinda works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/teleops/newteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/teleops/newteleop.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.DECODE.teleops;
 
 import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
@@ -17,6 +18,8 @@ import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.ftc.NextFTCOpMode;
 import static org.firstinspires.ftc.teamcode.DECODE.PIDs.flywheelpid.*;
 import static dev.nextftc.bindings.Bindings.button;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 @TeleOp(name = "newtelop")
@@ -39,16 +42,32 @@ public class newteleop extends NextFTCOpMode {
 
     int intaekstage =-1 , shooterstage =-1;
 
+    double y;
+    double x ;
+    double rx ;
+
+    // Denominator is the largest motor power (absolute value) or 1
+    // This ensures all the powers maintain the same ratio,
+    // but only if at least one is out of the range [-1, 1]
+    double denominator ;
+    double frontLeftPower;
+    double backLeftPower ;
+    double frontRightPower ;
+    double backRightPower ;
+
+    GoBildaPinpointDriver pinpoint;
 
 
-
-    Button gamepad1a = button(() -> gamepad1.a);
+    Button gamepad1y = button(() -> gamepad1.y);
     Button gamepad1b = button(() -> gamepad1.b);
     Button gamepad1x = button(() -> gamepad1.x);
+    Button shootingfsm = button(() -> gamepad2.right_bumper);
+
 
     DcMotorEx FL, FR, BL, BR, leftinake, rightinake;
     Servo flicky;
 
+    double botHeading;
 
 
     @Override
@@ -67,15 +86,23 @@ public class newteleop extends NextFTCOpMode {
         rightspindex = hardwareMap.get(Servo.class, "rightspindex");
         intake = hardwareMap.get(DcMotorEx.class, "Lintake");
 
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+
+        // Configure the sensor
 
 
         waitForStart();
         colorSensor.setGain(12);
 
         while(opModeIsActive()) {
-            NormalizedRGBA colors = colorSensor.getNormalizedColors();
             greenv = colors.green;
             bluev = colors.blue;
+            pinpoint.update();
+            botHeading = pinpoint.getHeading(AngleUnit.DEGREES);
+
+
 
             if (greenv > 0.02 && greenv < 0.04 && bluev > 0.02 && bluev < 0.039) {
                 move = true;
@@ -87,7 +114,7 @@ public class newteleop extends NextFTCOpMode {
 
             //get data from hub; store as variables at beginning of loop
 
-            gamepad1a.whenBecomesTrue(() -> configvelocity = 1267); //rough near zone
+            gamepad1y.whenBecomesTrue(() -> configvelocity = 1267); //rough near zone
 
             gamepad1b.whenBecomesTrue(() -> configvelocity = 1520); //rough far zone
 
@@ -149,7 +176,6 @@ public class newteleop extends NextFTCOpMode {
 
 
 
-
             switch (shooterstage) {
                 case 0:
 //                    rotationpos = 0.6167;
@@ -182,53 +208,43 @@ public class newteleop extends NextFTCOpMode {
             }
 
 
-            shooter();
 
-//            if (gamepad2.a) {
-//                BR.setPower(1);
-//            } else if (gamepad2.b) {
-//                BL.setPower(1);
-//            }
-//            else if (gamepad2.x) {
-//                FR.setPower(1);
-//            }
-//            else if (gamepad2.y) {
-//                FL.setPower(1);
-//            } else if (gamepad2.left_bumper) {
-//                BR.setPower(-1);
-//                FR.setPower(-1);
-//                FL.setPower(1);
-//                BL.setPower(1);
-//            } else{
-//                BR.setPower(0);
-//                FR.setPower(0);
-//                FL.setPower(0);
-//                BL.setPower(0);
-//            }
+
             double y = -gamepad2.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad2.left_stick_x * 1.1; // Counteract imperfect strafing
+            double x = gamepad2.left_stick_x;
             double rx = gamepad2.right_stick_x;
+
+            if (gamepad1.options) {
+                pinpoint.setHeading(0, AngleUnit.DEGREES);
+            }
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x - rx) / denominator;
-            double frontRightPower = (y - x + rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+
+            shooter();
+
 
             FL.setPower(frontLeftPower);
             BL.setPower(backLeftPower);
             FR.setPower(frontRightPower);
             BR.setPower(backRightPower);
 
-//
-
             telemetry.addData("output real velocity:", flywheelvelocity);
             telemetry.addData("input velocity:", configvelocity);
             telemetry.update();
-
 //claire skibidi toilet ohio
 
         }
@@ -242,3 +258,9 @@ public class newteleop extends NextFTCOpMode {
 //heading lock via ll/pp
 //indexing
 //formula for flywheel speed
+
+
+//driver: field centric joysticks, shooter mode on/off (HOLD to keep shooting, not just press)
+//opperator: *heading lock on/off*, shooter speed, intake mode on/off,
+//make single driver code for funsies (later)
+//MONDAY -- FINISH TELEOP BY ADDING HEADING LOCK, MAKING SHOOTING A HOLD BUTTON ( NOT PRESS BUTTON), TUNE PEDRO
